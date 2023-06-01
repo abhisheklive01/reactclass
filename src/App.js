2cda92d38b800a4859a64d03ebf6245e3219c041
abhi <manyypal@gmail.ccom>
2023-06-01 19:54:57 +0530

    01-06-2023

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import Demo from './demo';
+import FormState from './FormState';
+import ApiCall from './ApiCall';
+import AddData from './AddData';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import SingleUser from './SingleUser';
+import FomData from './FomData';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<AddData />}/>        
+        <Route path="/singleuser/:id" element={<SingleUser/>} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/ProsClass.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function ProsClass(props) {
+  return (
+    <div>
+   
+    <p>First Name : {props.firstName}</p>
+   <p>Age : {props.age}</p>
+    
+    </div>
+  )
+}
